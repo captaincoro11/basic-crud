53ca1e87ae3669e763bde827d4b4e9c4477e839c
captaincoro11 <captaincoro444@gmail.com>
2024-05-26 14:44:01 +0530

    le

--- server/api/index.js
+++ server/api/index.js
@@ -12,7 +12,8 @@ dotenv.config({});
 
 connectDatabase();
 app.use(cors());
-app.use(bodyParser.json())
+app.use(express.json({limit:"50mb"}))
+app.use(express.static('public'))
 app.use('/user',userRoutes)
 app.use('/task',taskRoutes)
 
